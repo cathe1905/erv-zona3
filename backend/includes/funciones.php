23b7ea71700b71f18714f8dc87b72250a32c984e
catherin <catherinr24@gmail.com>
2025-02-11 12:02:05 -0300

    actualizando con el hosting

--- backend/includes/funciones.php
+++ backend/includes/funciones.php
@@ -209,4 +209,15 @@ function deleteRecord($class, $type){
     }
 }
 
+function loadEnv($filePath) {
+    if (file_exists($filePath)) {
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+            [$key, $value] = explode('=', $line, 2);
+            putenv("$key=$value");
+        }
+    }
+}
+
 
